55d9a63ec7dfff01514565a1faf33a3e81819a29
Yonglie Lu <YLu27@slb.com>
2022-12-28 13:31:34 +0800

    sdf

--- docs/question/core.js
+++ docs/question/core.js
@@ -3,30 +3,56 @@ let currentPlayAudio = null;
 let errorInfoBox = null;
 let maxAudioNumber = 4;
 let lastElement = null;
-currentPlayNumber = 0;
-var clickAudioUrl =
+let currentPlayNumber = 0;
+let clickAudioUrl =
     "https://vod.ruotongmusic.com/sv/371bb841-179ccf90325/371bb841-179ccf90325.wav";
-var clickAudio = [];
+let clickAudio = [];
 for (let index = 0; index < maxAudioNumber; index++) {
-    var temp = new Audio(clickAudioUrl);
+    let temp = new Audio(clickAudioUrl);
     temp.errorSource = "点击声效" + index;
+    temp.onabort = function () {
+        temp = reCreateAudio(temp);
+    };
     clickAudio.push(temp);
 }
 
 
-var successAudio = new Audio(
+let successAudio = new Audio(
     "https://vod.ruotongmusic.com/sv/28e211ba-179ccca0dfa/28e211ba-179ccca0dfa.wav"
 );
 successAudio.errorSource = "成功声效";
-var failAudio = new Audio(
+successAudio.onabort = function () {
+    successAudio = reCreateAudio(successAudio);
+
+};
+function reCreateAudio(audio) {
+    let newAudio = new Audio(audio.src);
+    let errorCount = audio.errorCount;
+    newAudio.errorSource = audio.errorSource;
+    newAudio.tag = "-异常重建";
+    if (!errorCount) {
+        errorCount = 1;
+    }
+    else {
+        errorCount += 1;
+    }
+    newAudio.errorCount = errorCount;
+
+    return newAudio;
+}
+let failAudio = new Audio(
     "https://vod.ruotongmusic.com/sv/5dc38d4-179ccc95e2f/5dc38d4-179ccc95e2f.wav"
 );
 failAudio.errorSource = "错误声效";
+failAudio.onabort = function () {
+    failAudio = reCreateAudio(failAudio);
+
+};
 function randomSort(a, b) {
     return Math.random() > 0.5 ? -1 : 1;
 }
 function removeEmpty(array) {
-    var newArray = [];
+    let newArray = [];
     array.forEach((v) => {
         if (v) {
             newArray.push(v);
@@ -44,12 +70,12 @@ window.onerror = function (error) {
     console.error(error)
 }
 function createQuestion(data) {
-    var article = document.getElementsByTagName("article")[0];
-    var h1 = document.createElement("h4");
+    let article = document.getElementsByTagName("article")[0];
+    let h1 = document.createElement("h4");
     h1.innerText = "句型练习";
-    var ol = document.createElement("ol");
+    let ol = document.createElement("ol");
     data.forEach((oneQuestion) => {
-        var question = createOneQuestion(oneQuestion);
+        let question = createOneQuestion(oneQuestion);
         ol.appendChild(question);
     });
 
@@ -58,12 +84,12 @@ function createQuestion(data) {
 }
 
 function createDialog(data) {
-    var article = document.getElementsByTagName("article")[0];
-    var h1 = document.createElement("h4");
+    let article = document.getElementsByTagName("article")[0];
+    let h1 = document.createElement("h4");
     h1.innerText = "对话练习";
-    var ol = document.createElement("ol");
+    let ol = document.createElement("ol");
     data.forEach((oneDialog) => {
-        var question = createOneDialog(oneDialog);
+        let question = createOneDialog(oneDialog);
         ol.appendChild(question);
     });
 
@@ -72,8 +98,8 @@ function createDialog(data) {
 }
 
 function createHtmlContents(contents) {
-    var article = document.getElementsByTagName("article")[0];
-    var count = article.childElementCount;
+    let article = document.getElementsByTagName("article")[0];
+    let count = article.childElementCount;
     for (let index = 0; index < count; index++) {
         article.removeChild(article.children[0]);
 
@@ -86,17 +112,17 @@ function createHtmlContents(contents) {
 
 
 function createOneQuestion(d) {
-    var li = document.createElement("li");
+    let li = document.createElement("li");
 
-    var ul = document.createElement("ul");
+    let ul = document.createElement("ul");
     li.appendChild(ul);
-    var li_title = document.createElement("li");
+    let li_title = document.createElement("li");
     li_title.textContent = d.title;
-    var li_QuestionParent = document.createElement("li");
-    var li_action = document.createElement("li");
-    var li_result = document.createElement("li");
-    var li_words = document.createElement("li");
-    var li_Question = document.createElement("div");
+    let li_QuestionParent = document.createElement("li");
+    let li_action = document.createElement("li");
+    let li_result = document.createElement("li");
+    let li_words = document.createElement("li");
+    let li_Question = document.createElement("div");
     li_QuestionParent.appendChild(li_Question);
     li_Question.className = "select-word";
     createResetButton(li_action, li_Question, li_words);
@@ -119,24 +145,24 @@ function createOneQuestion(d) {
     return li;
 }
 function createOneDialog(d) {
-    var li = document.createElement("li");
+    let li = document.createElement("li");
 
-    var ul = document.createElement("ul");
+    let ul = document.createElement("ul");
     li.appendChild(ul);
 
     d.title.forEach((title) => {
-        var li_title1 = document.createElement("li");
+        let li_title1 = document.createElement("li");
         li_title1.textContent = title;
         ul.appendChild(li_title1);
     });
     d.answer.forEach((answer) => {
         let li_words = document.createElement("li");
         let li_QuestionParent = document.createElement("li");
-        var li_Question = document.createElement("div");
+        let li_Question = document.createElement("div");
         li_QuestionParent.appendChild(li_Question);
         li_Question.className = "select-word";
         let li_action = document.createElement("li");
-        var li_result = document.createElement("li");
+        let li_result = document.createElement("li");
         createWordButton(
             answer.words,
             answer.en,
@@ -160,24 +186,24 @@ function createOneHtmlContent(d, parent) {
 
     if (d.tagName) {
         d.imgSrc.forEach(url => {
-            var imgElement = document.createElement("IMG");
+            let imgElement = document.createElement("IMG");
             imgElement.src = url;
             parent.appendChild(imgElement);
         });
-        var element = document.createElement(d.tagName);
+        let element = document.createElement(d.tagName);
         element.textContent = d.content;
         parent.appendChild(element);
     } else if (d.answer) {
 
         handlerDialogEnWord(d, "answer");
-        var ul = document.createElement("ul");
-        var li_title = document.createElement("li");
+        let ul = document.createElement("ul");
+        let li_title = document.createElement("li");
         li_title.textContent = d.title;
-        var li_QuestionParent = document.createElement("li");
-        var li_action = document.createElement("li");
-        var li_result = document.createElement("li");
-        var li_words = document.createElement("li");
-        var li_Question = document.createElement("div");
+        let li_QuestionParent = document.createElement("li");
+        let li_action = document.createElement("li");
+        let li_result = document.createElement("li");
+        let li_words = document.createElement("li");
+        let li_Question = document.createElement("div");
         li_QuestionParent.appendChild(li_Question);
         li_Question.className = "select-word";
         createResetButton(li_action, li_Question, li_words);
@@ -197,19 +223,19 @@ function createOneHtmlContent(d, parent) {
         ul.appendChild(li_action);
         ul.appendChild(li_result);
         if (d.describe) {
-            var li_describe = document.createElement("li");
+            let li_describe = document.createElement("li");
             li_describe.textContent = d.describe;
             ul.appendChild(li_describe);
         }
         parent.appendChild(ul);
     } else if (d.yinbiao) {
-        var li_word = document.createElement("p");
-        var li_strong = document.createElement("strong");
+        let li_word = document.createElement("p");
+        let li_strong = document.createElement("strong");
         li_strong.textContent = d.word;
         li_word.appendChild(li_strong);
         li_word.appendChild(document.createTextNode(d.yinbiao));
 
-        var li_a = document.createElement("a");
+        let li_a = document.createElement("a");
         li_a.href = "https://dict.youdao.com/dictvoice?audio=" + d.word + "&type=2";
         li_a.textContent = "播放";
         li_word.appendChild(li_a);
@@ -227,7 +253,7 @@ function createWordButton(
     li_result
 ) {
     words.forEach((word) => {
-        var button = document.createElement("button");
+        let button = document.createElement("button");
         button.innerText = word;
         button.onclick = function () {
             let audio = clickAudio[currentPlayNumber];
@@ -242,7 +268,7 @@ function createWordButton(
                 li_Question.lastButtons = [];
             }
             if (button.oldValue === undefined) {
-                var li_word = document.createElement("span");
+                let li_word = document.createElement("span");
                 li_word.textContent = word;
                 if (!button.relationId) {
                     button.relationId = guid();
@@ -253,13 +279,13 @@ function createWordButton(
                 li_Question.lastButtons.push(button);
                 li_Question.appendChild(li_word);
             } else {
-                var lastButton =
+                let lastButton =
                     li_Question.lastButtons[li_Question.lastButtons.length - 1];
                 if (lastButton == button) {
                     li_Question.lastButtons.length -= 1;
                     button.oldValue = undefined;
                     button.className = "";
-                    var relation = document.getElementById(button.relationId);
+                    let relation = document.getElementById(button.relationId);
                     if (relation) relation.remove();
                 }
             }
@@ -269,29 +295,20 @@ function createWordButton(
         parent.appendChild(button);
     });
 }
+
 function playAudion(audio, onError) {
     if (audio.duration) {
         audio.currentTime = 0;
     }
     audio.canPause = false;
-    var playPromise = audio.play();
+    let playPromise = audio.play();
     if (playPromise !== undefined) {
         playPromise.then(_ => {
             audio.canPause = true;
         }).catch(error => {
-            console.error("来自播放音频错误[" + audio.errorSource + "]:");
-            console.error(error);
+            console.error("来自播放音频第" + (audio.errorCount ? audio.errorCount : 0) + "次错误[" + audio.errorSource + audio.tag + "]:");
             if (onerror) {
                 onError();
-            } else {
-
-                setTimeout(() => {
-                    var errorSource = audio.errorSource;
-                    audio = new Audio(audio.src);
-                    audio.errorSource = errorSource + "-异常重试";
-                    playAudion(audio);
-                }, 500)
-
             }
         });
     }
@@ -321,7 +338,7 @@ function playAudionWithUrl(url, loop, onError) {
     playAudion(audio, onError);
 }
 function createPlayLink(liparent, words) {
-    var link_play = document.createElement("a");
+    let link_play = document.createElement("a");
     link_play.textContent = "播放";
     link_play.href =
         "https://dict.youdao.com/dictvoice?audio=" +
@@ -330,7 +347,7 @@ function createPlayLink(liparent, words) {
     liparent.appendChild(link_play);
 }
 function createResetButton(liparent, li_Question, li_words) {
-    var btn_reset = document.createElement("button");
+    let btn_reset = document.createElement("button");
     btn_reset.textContent = "重置";
     btn_reset.onclick = function () {
         li_Question.lastButtons.forEach((element) => {
@@ -365,7 +382,7 @@ function check(
         li_result.textContent = "";
         return;
     }
-    var test = undefined;
+    let test = undefined;
     for (let index = 0; index < resultElements.childElementCount; index++) {
         const element = resultElements.children[index];
 
@@ -375,13 +392,13 @@ function check(
             test += " " + element.innerText;
         }
     }
-    var isSuccess = test == answer;
+    let isSuccess = test == answer;
     if (isSuccess) {
         li_result.textContent = "正确";
         li_result.className = "success";
         playAudion(successAudio);
         setTimeout(() => {
-            var href =
+            let href =
                 "https://dict.youdao.com/dictvoice?audio=" +
                 originalWords.join("+") +
                 "&le=eng&le=eng&type=2";
@@ -435,7 +452,7 @@ function playOne(element) {
 }
 function handlerDialogEnWord(d, propertyName) {
     d[propertyName] = d[propertyName].trim();
-    var words = d[propertyName].split(/\s/);
+    let words = d[propertyName].split(/\s/);
     words = removeEmpty(words);
     words.sort(randomSort);
     d.words = words;
@@ -444,14 +461,14 @@ function handlerDialogEnWord(d, propertyName) {
 }
 
 function createNextLink() {
-    var nav = document.getElementsByClassName("sidebar-nav")[0];
+    let nav = document.getElementsByClassName("sidebar-nav")[0];
     let allLinks = nav.getElementsByTagName("a");
     if (allLinks.length == 0) {
 
         setTimeout(v => createNextLink(), 500);
     } else {
-        var total = allLinks.length;
-        var currentUrl = document.location.href;
+        let total = allLinks.length;
+        let currentUrl = document.location.href;
         for (let index = 0; index < total; index++) {
             const element = allLinks[index];
             if (currentUrl == element.href) {
@@ -462,7 +479,7 @@ function createNextLink() {
                     nextLink.href = nextLinkUrl;
                     nextLink.target = "_self";
                     nextLink.textContent = "下一节";
-                    var article = document.getElementsByTagName("article")[0];
+                    let article = document.getElementsByTagName("article")[0];
                     article.appendChild(nextLink);
                     createErrorInfoBox();
                     return;
@@ -482,7 +499,7 @@ function createErrorInfoBox() {
     errorInfoBox = document.createElement("textarea");
     errorInfoBox.rows = "10";
     errorInfoBox.value = "程序错误信息：\r\n";
-    var article = document.getElementsByTagName("article")[0];
+    let article = document.getElementsByTagName("article")[0];
 
     article.appendChild(document.createElement("br"));
     article.appendChild(document.createElement("br"));
@@ -490,7 +507,7 @@ function createErrorInfoBox() {
 
 }
 function playLinkClick(srcElement) {
-    var innerText = srcElement.innerText;
+    let innerText = srcElement.innerText;
     if (innerText == "播放") {
         srcElement.onclick = function (e) {
             e.preventDefault();
@@ -540,12 +557,12 @@ window.onload = function () {
         }
     });
 
-    var lastTouchEnd = 0;
+    let lastTouchEnd = 0;
 
     document.addEventListener(
         "touchend",
         function (event) {
-            var now = new Date().getTime();
+            let now = new Date().getTime();
             if (now - lastTouchEnd <= 300) {
                 event.preventDefault();
             }
@@ -571,26 +588,26 @@ onmousedown = function (event) {
         return;
     }
     event.preventDefault();
-    var innerText = event.srcElement.innerText;
-    var isPlayLink = playLinkClick(event.srcElement);
+    let innerText = event.srcElement.innerText;
+    let isPlayLink = playLinkClick(event.srcElement);
     if (isPlayLink) {
         return false;
     }
-    var url = event.srcElement.href;
+    let url = event.srcElement.href;
     if (innerText == "下一节") {
         return reloadByUrl(url);
     }
 
     else {
 
-        var localUrl = document.location.href;
+        let localUrl = document.location.href;
         if (localUrl.endsWith("/#/")) {
             return reloadByUrl(url);
 
         } else {
 
 
-            var rootTagName = event.srcElement.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.tagName;
+            let rootTagName = event.srcElement.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.parentElement?.tagName;
             if (rootTagName == "ASIDE") {
                 return reloadByUrl(url);
             }

a5d6d39f8fd1313c880a843e14c625517a91cffc
Yonglie Lu <YLu27@slb.com>
2023-01-14 16:38:35 +0800

    Sdf

--- docs/script/audioutils.js
+++ docs/script/audioutils.js
@@ -1,37 +1,23 @@
 
-let tempAudio = null;
+
 let currentPlayAudio = null;
-let maxAudioNumber = 14;
+
 let MaxTryPlayCount = 10;
 let lastElement = null;
-let clickAudio = [];
-let clickAudioIndex = -1;
-let clickAudioUrl =
-    "https://vod.ruotongmusic.com/sv/371bb841-179ccf90325/371bb841-179ccf90325.wav";
-function createAudio(url, source) {
-    let temp = new Audio(url);
-    temp.preload = "auto";
-    temp.errorSource = source.replace("https://dict.youdao.com/dictvoice?audio=", "").replace("&le=eng&le=eng&type=2", "").replace("&le=eng&le=eng&type=1", "");
-    temp.errorCount = 0;
-    temp.tag = "";
-    temp.canPause = false;
 
-    let logInfo = temp.errorSource + temp.tag;
-    temp.canPlayThisAudio = false;
-    temp.tryPlayCount = 0;
-    temp.onloadedmetadata = function () {
-        // console.log("audio loadedmetadata:" + logInfo);
-        temp.canPlayThisAudio = true;
-    }
-    temp.load();
-    temp.isLoad = true;
+function createAudio(url, audioSrc, loop) {
+    let temp = new Howl({
+        src: [url],
+        preload: true,
+        loop: loop === true,
+        html5: true, // 设置为true 页面将使用原生video 标签渲染 不会导致资源跨域的情况
+
+    });
     return temp;
 }
-for (let index = 0; index < maxAudioNumber; index++) {
-
-    clickAudio.push(createAudio(clickAudioUrl, "点击声效" + index));
-}
-
+let clickAudio = createAudio(
+    "https://vod.ruotongmusic.com/sv/371bb841-179ccf90325/371bb841-179ccf90325.wav", "点击声效"
+);
 
 let successAudio = createAudio(
     "https://vod.ruotongmusic.com/sv/28e211ba-179ccca0dfa/28e211ba-179ccca0dfa.wav", "成功声效"
@@ -42,69 +28,27 @@ let failAudio = createAudio(
 );
 
 
- function playAudion(audio) {
-
-    if (!audio.isLoad) {
-        audio.canPlayThisAudio = false;
-        audio.canPause = false;
-        audio.load();
-        audio.isLoad = true;
-    }
-
-    let logInfo = audio.errorSource + audio.tag
-    if (!audio.canPlayThisAudio) {
-        audio.tryPlayCount += 1;
-        //console.log("["+audio.tryPlayCount+"]start play,but can't be play ,wait a moment:" + logInfo);
-
-        if (audio.tryPlayCount > MaxTryPlayCount) {
-            console.log("放弃 try :" + MaxTryPlayCount);
-            return;
-        }
-        setTimeout(() => playAudion(audio), 500);
-        ;
-        return;
-    }
-    //  console.log("start play audio.duration:" + audio.duration + audio.canPause);
-    if (audio.duration) {
-        if (audio.canPause) {
-            audio.pause();
-        }
-        audio.currentTime = 0;
-    }
-    audio.canPause = false;
-    audio.isLoad = false;
-    audio.tryPlayCount = 0;
-     audio.play().then(_ => {
-        audio.canPause = true;
-
-        //  console.log("start play succecee:" + logInfo);
-    }).catch(error => {
-        console.log("start play error:" + logInfo);
-        console.error(error)
-        let source = temp.errorSource;
-        console.error("来自播放音频第" + (audio.errorCount ? audio.errorCount : 0) + "次错误[" + source + (audio.tag || "") + "]:");
-        stopPlay();
-
-    });
-
+function playAudion(audio) {
+    audio.play();
 }
-
 function playAudionWithUrl(url, loop) {
     if (!loop) {
         stopPlay();
     }
-    currentPlayAudio = createAudio(url, "播放音频：" + url);
-    currentPlayAudio.loop = loop;
-
+    unloadTempAudion();
+    currentPlayAudio = createAudio(url, "播放音频：" + url, loop);
     playAudion(currentPlayAudio);
 }
-
+function unloadTempAudion() {
+    if (currentPlayAudio) {
+        currentPlayAudio.unload();
+        currentPlayAudio = undefined;
+    }
+}
 function stopPlay() {
     if (!lastElement) return;
     lastElement.innerText = lastElement.oldText;
-    if (currentPlayAudio != null && currentPlayAudio.canPause) {
-        currentPlayAudio.pause();
-    }
+    unloadTempAudion();
 }
 
 function playOne(element) {
@@ -118,16 +62,9 @@ function playOne(element) {
 
 
 function playAlertAudio() {
-
-    let index = ++clickAudioIndex;
-    if (index == maxAudioNumber - 1) {
-
-        clickAudioIndex = -1;
-    }
-    playAudion(clickAudio[index]);
+    playAudion(clickAudio);
 }
 function playSuccessAudio() {
-
 
     playAudion(successAudio);
 }
